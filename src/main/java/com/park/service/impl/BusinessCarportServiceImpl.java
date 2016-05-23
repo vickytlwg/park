d55bfb65742f0d9a2017b555448d330690903f8a
ruoxingzpc <zhangpc1987@163.com>
2016-05-23 08:25:20 -0400

    fix bug

--- src/main/java/com/park/service/impl/BusinessCarportServiceImpl.java
+++ src/main/java/com/park/service/impl/BusinessCarportServiceImpl.java
@@ -220,7 +220,7 @@ public class BusinessCarportServiceImpl implements BusinessCarportService{
 			usage = usage + carportEnd.getTime() - carportStart.getTime();
 		}
 		
-		double rate = usage / (endDay.getTime() - startDay.getTime()) * 1.0;
+		double rate = usage / ((endDay.getTime() - startDay.getTime()) * 1.0);
 		return rate;
 	}
 
