2b0b1b6c446d7b037aff2d0e79f2ba9dd25020c9
liumeng <liushuijiwusi@163.com>
2018-06-05 21:14:57 +0800

    修正

--- src/main/java/com/park/controller/PosChargeDataController.java
+++ src/main/java/com/park/controller/PosChargeDataController.java
@@ -30,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.alipay.api.AlipayApiException;
 import com.jpush.Jpush;
 import com.park.dao.CarportStatusDetailDAO;
