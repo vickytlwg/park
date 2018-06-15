4d140782c035a1d76d4f4a991ef1a4a6fbc1da41
liumeng <liushuijiwusi@163.com>
2018-06-15 10:37:57 +0800

    k

--- src/main/java/com/park/controller/OutsideParkStatusController.java
+++ src/main/java/com/park/controller/OutsideParkStatusController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.park.model.AuthUser;
@@ -66,6 +67,35 @@ public class OutsideParkStatusController {
 		List<Park> parkl = new ArrayList<>();
 		for (Park park : parkList) {
 			if (park.getType()==3) {
+				parkl.add(park);
+			}
+		}
+		modelMap.addAttribute("parks", parkl);
+		AuthUser user = authService.getUserByUsername(username);
+		if(user != null){
+			modelMap.addAttribute("user", user);
+			boolean isAdmin = false;
+			if(user.getRole() == AuthUserRole.ADMIN.getValue())
+				isAdmin=true;
+			modelMap.addAttribute("isAdmin", isAdmin);
+			
+			Set<Page> pages = pageService.getUserPage(user.getId()); 
+			for(Page page : pages){
+				modelMap.addAttribute(page.getPageKey(), true);
+			}
+		}
+		return "outsideParkStatusAdmin";
+	}
+	
+	@RequestMapping(value="/outsideParkStatusAdminWithPark/{parkId}/")
+	public String outsideParkStatusAdminWithPark(@PathVariable("parkId")int parkId,ModelMap modelMap, HttpServletRequest request, HttpSession session){
+		String username = (String) session.getAttribute("username");
+		List<Park> parkList = parkService.getParks();
+		if(username != null)
+			parkList = parkService.filterPark(parkList, username);
+		List<Park> parkl = new ArrayList<>();
+		for (Park park : parkList) {
+			if (park.getId()==parkId) {
 				parkl.add(park);
 			}
 		}
