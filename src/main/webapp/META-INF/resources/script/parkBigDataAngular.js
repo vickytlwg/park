6561156188681388edb92d9c2e444c6e77025c38
vickytlwg <1282356934@qq.com>
2018-09-25 15:17:18 +0800

    Signed-off-by: vickytlwg <1282356934@qq.com>

--- src/main/webapp/META-INF/resources/script/parkBigDataAngular.js
+++ src/main/webapp/META-INF/resources/script/parkBigDataAngular.js
@@ -1,5 +1,5 @@
-angular.module("parkBigDataApp", []).
-controller("parkBigDataCtrl", ['$scope','$interval','httpService',
+var app=angular.module("parkBigDataApp", []);
+app.controller("parkBigDataCtrl", ['$scope','$interval','httpService',
 function($scope,$interval,httpService) {
 var data={low:0,count:10};
 $scope.items={};
@@ -24,8 +24,8 @@ $interval(function(){
     $scope.items=response;
 });
 },2000);
-}])
-.service("httpService",function($http,$q){
+}]);
+app.service("httpService",function($http,$q){
     return {
         getPosData:function(data){
             var deferred=$q.defer();
